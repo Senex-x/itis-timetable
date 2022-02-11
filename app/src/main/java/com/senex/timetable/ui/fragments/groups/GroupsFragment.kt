fd7d39a7a568e95bc95bfbedf7812fc3c4a047b8
V-Senex <junior.07.inator@gmail.com>
2022-02-11 23:33:10 +0300

    Add navigation via toolbar buttons

--- app/src/main/java/com/senex/timetable/ui/fragments/groups/GroupsFragment.kt
+++ app/src/main/java/com/senex/timetable/ui/fragments/groups/GroupsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.senex.timetable.R
 import com.senex.timetable.databinding.FragmentGroupsBinding
 
 class GroupsFragment : Fragment() {
@@ -27,6 +29,21 @@ class GroupsFragment : Fragment() {
         view: View,
         savedInstanceState: Bundle?,
     ): Unit = with(binding) {
+        groupsToolbar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.action_open_table -> {
+                    navigateToGroupsFragment()
+                    true
+                }
+                else -> false
+            }
 
+        }
+    }
+
+    private fun navigateToGroupsFragment() {
+        findNavController().navigate(
+            R.id.action_groupsFragment_to_tableFragment
+        )
     }
 }
--- app/src/main/java/com/senex/timetable/ui/fragments/table/TableFragment.kt
+++ app/src/main/java/com/senex/timetable/ui/fragments/table/TableFragment.kt
@@ -1,11 +1,13 @@
 package com.senex.timetable.ui.fragments.table
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.senex.timetable.R
 import com.senex.timetable.databinding.FragmentTableBinding
+import com.senex.timetable.utils.log
 
 class TableFragment : Fragment() {
     private var _binding: FragmentTableBinding? = null
@@ -26,7 +28,22 @@ class TableFragment : Fragment() {
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
-    ) = with(binding) {
+    ): Unit = with(binding) {
+        tableToolbar.setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.action_pick_group -> {
+                    navigateToTableFragment()
+                    true
+                }
+                else -> false
+            }
 
+        }
+    }
+
+    private fun navigateToTableFragment() {
+        findNavController().navigate(
+            R.id.action_tableFragment_to_groupsFragment
+        )
     }
 }
