4a6a70a2c5b10d2c7f7ff74371598e62020c27b8
V-Senex <junior.07.inator@gmail.com>
2022-02-25 12:37:52 +0300

    Add Apache POI library

--- app/src/main/java/com/senex/timetable/MainActivity.kt
+++ app/src/main/java/com/senex/timetable/MainActivity.kt
@@ -2,7 +2,11 @@ package com.senex.timetable
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.senex.timetable.common.log
 import com.senex.timetable.databinding.ActivityMainBinding
+import com.senex.timetable.parser.Parser
+import java.io.File
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -16,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun doOptionalStuff() {
+
         /*
         subjectRepository.getAllByGroupIdAndDayNumber(1, 1)
             .observe(this) {
