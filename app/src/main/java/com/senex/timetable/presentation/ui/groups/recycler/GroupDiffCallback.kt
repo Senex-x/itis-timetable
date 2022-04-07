0d33759d12e28a499821c8afc793fdd56b6c4bcc
V-Senex <junior.07.inator@gmail.com>
2022-04-07 12:03:10 +0300

    Refactor `GroupDiffCallback` `if` statements to `when`

--- app/src/main/java/com/senex/timetable/presentation/ui/groups/recycler/GroupDiffCallback.kt
+++ app/src/main/java/com/senex/timetable/presentation/ui/groups/recycler/GroupDiffCallback.kt
@@ -1,39 +1,39 @@
 package com.senex.timetable.presentation.ui.groups.recycler
 
 import androidx.recyclerview.widget.DiffUtil
-import com.senex.timetable.presentation.ui.groups.recycler.items.CourseRecyclerItem
 import com.senex.timetable.presentation.common.recycler.TypedRecyclerItem
-import com.senex.timetable.presentation.ui.groups.recycler.items.GroupRecyclerItemType
+import com.senex.timetable.presentation.ui.groups.recycler.items.CourseRecyclerItem
 import com.senex.timetable.presentation.ui.groups.recycler.items.GroupRecyclerItem
+import com.senex.timetable.presentation.ui.groups.recycler.items.GroupRecyclerItemType
 
 object GroupDiffCallback : DiffUtil.ItemCallback<TypedRecyclerItem>() {
     override fun areItemsTheSame(
         oldItem: TypedRecyclerItem,
         newItem: TypedRecyclerItem,
-    ) = if (oldItem.getViewType() == GroupRecyclerItemType.COURSE.value
-        && newItem.getViewType() == GroupRecyclerItemType.COURSE.value
-    ) {
-        (oldItem as CourseRecyclerItem).courseNumber == (newItem as CourseRecyclerItem).courseNumber
-    } else if (oldItem.getViewType() == GroupRecyclerItemType.GROUP.value
-        && newItem.getViewType() == GroupRecyclerItemType.GROUP.value
-    ) {
-        (oldItem as GroupRecyclerItem).group.id == (newItem as GroupRecyclerItem).group.id
-    } else {
-        false
+    ) = when (oldItem) {
+        is CourseRecyclerItem ->
+            newItem is CourseRecyclerItem
+                    && oldItem.courseNumber == newItem.courseNumber
+
+        is GroupRecyclerItem ->
+            newItem is GroupRecyclerItem
+                    && oldItem.group.id == newItem.group.id
+
+        else -> false
     }
 
     override fun areContentsTheSame(
         oldItem: TypedRecyclerItem,
         newItem: TypedRecyclerItem,
-    ) = if (oldItem.getViewType() == GroupRecyclerItemType.COURSE.value
-        && newItem.getViewType() == GroupRecyclerItemType.COURSE.value
-    ) {
-        (oldItem as CourseRecyclerItem).courseNumber == (newItem as CourseRecyclerItem).courseNumber
-    } else if (oldItem.getViewType() == GroupRecyclerItemType.GROUP.value
-        && newItem.getViewType() == GroupRecyclerItemType.GROUP.value
-    ) {
-        (oldItem as GroupRecyclerItem).group == (newItem as GroupRecyclerItem).group
-    } else {
-        false
+    ) = when (oldItem) {
+        is CourseRecyclerItem ->
+        newItem is CourseRecyclerItem
+                && oldItem.courseNumber == newItem.courseNumber
+
+        is GroupRecyclerItem ->
+        newItem is GroupRecyclerItem
+                && oldItem.group == newItem.group
+
+        else -> false
     }
 }
