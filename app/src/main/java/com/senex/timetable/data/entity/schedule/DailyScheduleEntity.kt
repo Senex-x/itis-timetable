7e95a2d91860753c27a9f43723494c6f7466f0e1
V-Senex <junior.07.inator@gmail.com>
2022-04-12 23:10:14 +0300

    Add `EnglishSubjectEntity` to group together english subjects

--- app/src/main/java/com/senex/timetable/data/entity/schedule/DailyScheduleEntity.kt
+++ app/src/main/java/com/senex/timetable/data/entity/schedule/DailyScheduleEntity.kt
@@ -2,6 +2,7 @@ package com.senex.timetable.data.entity.schedule
 
 import androidx.room.Embedded
 import androidx.room.Relation
+import com.senex.timetable.data.entity.subject.EnglishSubjectEntity
 import com.senex.timetable.data.entity.subject.SubjectEntity
 import com.senex.timetable.data.entity.subject.VariedSubjectEntity
 
@@ -13,6 +14,11 @@ data class DailyScheduleEntity(
         entityColumn = "daily_schedule_id",
     )
     val variedSubjectEntities: List<VariedSubjectEntity>,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "daily_schedule_id",
+    )
+    val englishSubjectEntities: List<EnglishSubjectEntity>,
     @Relation(
         parentColumn = "id",
         entityColumn = "daily_schedule_id",
