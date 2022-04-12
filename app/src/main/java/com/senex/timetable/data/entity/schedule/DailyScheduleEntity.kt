5589f1736cd94b728fd689af41ca1c7f2829d8ab
V-Senex <junior.07.inator@gmail.com>
2022-04-12 23:07:27 +0300

    Add `VariedSubjectEntity` to group together elective subjects

--- app/src/main/java/com/senex/timetable/data/entity/schedule/DailyScheduleEntity.kt
+++ app/src/main/java/com/senex/timetable/data/entity/schedule/DailyScheduleEntity.kt
@@ -3,10 +3,16 @@ package com.senex.timetable.data.entity.schedule
 import androidx.room.Embedded
 import androidx.room.Relation
 import com.senex.timetable.data.entity.subject.SubjectEntity
+import com.senex.timetable.data.entity.subject.VariedSubjectEntity
 
 data class DailyScheduleEntity(
     @Embedded
     val dailyScheduleInfoEntity: DailyScheduleInfoEntity,
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "daily_schedule_id",
+    )
+    val variedSubjectEntities: List<VariedSubjectEntity>,
     @Relation(
         parentColumn = "id",
         entityColumn = "daily_schedule_id",
---
+++ app/src/main/java/com/senex/timetable/data/entity/subject/VariedSubjectEntity.kt
@@ -0,0 +1,25 @@
+package com.senex.timetable.data.entity.subject
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import com.senex.timetable.data.entity.schedule.DailyScheduleInfoEntity
+
+@Entity(
+    tableName = "varied_subjects",
+    foreignKeys = [
+        ForeignKey(
+            entity = DailyScheduleInfoEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["daily_schedule_id"],
+            onDelete = ForeignKey.CASCADE,
+        )
+    ],
+)
+data class VariedSubjectEntity(
+    @PrimaryKey
+    val id: Long,
+    @ColumnInfo(name = "daily_schedule_id")
+    val dailyScheduleId: Long,
+)
