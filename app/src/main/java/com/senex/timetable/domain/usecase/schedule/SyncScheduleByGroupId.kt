480ca64380da297409e1d1cbf043f8447469f7ac
V-Senex <junior.07.inator@gmail.com>
2022-05-30 14:49:52 +0300

    Disable `Schedule` syncing if there was no remote changes

--- app/src/main/java/com/senex/timetable/domain/usecase/schedule/SyncScheduleByGroupId.kt
+++ app/src/main/java/com/senex/timetable/domain/usecase/schedule/SyncScheduleByGroupId.kt
@@ -1,20 +1,53 @@
 package com.senex.timetable.domain.usecase.schedule
 
+import com.senex.timetable.domain.model.schedule.Schedule
 import com.senex.timetable.domain.usecase.group.DeleteGroupById
 import com.senex.timetable.domain.util.log
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class SyncScheduleByGroupId @Inject constructor(
     private val getRemoteScheduleByGroupId: GetRemoteScheduleByGroupId,
     private val deleteGroupById: DeleteGroupById,
     private val saveSchedule: SaveSchedule,
+    private val getScheduleByGroupIdSorted: GetScheduleByGroupIdSorted,
 ) {
     suspend operator fun invoke(groupId: Long) {
         getRemoteScheduleByGroupId(groupId)?.let {
-            deleteGroupById(groupId)
-            saveSchedule(it)
-            "Schedule sync done".log()
+            val localSchedule = getScheduleByGroupIdSorted(groupId).first()
+            if (localSchedule == null || !areSchedulesSameLazy(localSchedule, it)) {
+                deleteGroupById(groupId)
+                saveSchedule(it)
+                "Schedule sync done".log()
+            } else {
+                "Schedule sync canceled, no changes detected".log()
+            }
         }
+    }
+
+    private fun areSchedulesSameLazy(first: Schedule, second: Schedule): Boolean {
+        var result = false
+        for (firstDailySchedule in first.dailySchedules) {
+            for (secondDailySchedule in second.dailySchedules) {
+                for (firstSubject in firstDailySchedule.subjects) {
+                    for (secondSubject in secondDailySchedule.subjects) {
+                        result = firstSubject.dailyScheduleId == secondSubject.dailyScheduleId
+                                && firstSubject.electiveSubjectId == secondSubject.electiveSubjectId
+                                && firstSubject.englishSubjectId == secondSubject.englishSubjectId
+                                && firstSubject.indexInDay == secondSubject.indexInDay
+                                && firstSubject.startTime == secondSubject.startTime
+                                && firstSubject.endTime == secondSubject.endTime
+                                && firstSubject.name == secondSubject.name
+                                && firstSubject.room == secondSubject.room
+                                && firstSubject.type == secondSubject.type
+                                && firstSubject.kind == secondSubject.kind
+                                && firstSubject.teacherName == secondSubject.teacherName
+                                && firstSubject.teacherPatronymic == secondSubject.teacherPatronymic
+                                && firstSubject.teacherSurname == secondSubject.teacherSurname
+                    }
+                }
+            }
+        }
+        return result
     }
 }
