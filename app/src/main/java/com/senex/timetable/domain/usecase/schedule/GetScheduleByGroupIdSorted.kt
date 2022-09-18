75db0c75eaa9b0f058dd13269145b6e4627d5880
senex-x <vdm.snx@gmail.com>
2022-09-18 15:53:01 +0300

    Remove unused imports from `GetScheduleByGroupIdSorted` use case

--- app/src/main/java/com/senex/timetable/domain/usecase/schedule/GetScheduleByGroupIdSorted.kt
+++ app/src/main/java/com/senex/timetable/domain/usecase/schedule/GetScheduleByGroupIdSorted.kt
@@ -1,7 +1,5 @@
 package com.senex.timetable.domain.usecase.schedule
 
-import com.senex.timetable.domain.model.schedule.DailySchedule
-import com.senex.timetable.domain.model.schedule.Schedule
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
